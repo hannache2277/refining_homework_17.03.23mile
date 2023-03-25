5a0e30eaa4c0cbf9f8c7a3c26503656ce2c90c33
hannache2277 <hannache2277@gmail.com>
2023-03-25 06:56:54 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticetPrice = 150;
+        int oneBonusMile = 20;
+        int bonusMile = ticetPrice / oneBonusMile;
+
+        System.out.println("Итоговое количество бонусных миль = " + bonusMile);
+    }
+}
